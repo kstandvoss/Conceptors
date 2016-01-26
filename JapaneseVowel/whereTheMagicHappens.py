3f47022d56ed5c89f4c79a0f811311c70f429d83
kstandvoss <kstandvoss@uos.de>
2016-01-26 14:12:03 +0100

    deleted duplicate classes

--- JapaneseVowel/whereTheMagicHappens.py
+++ JapaneseVowel/whereTheMagicHappens.py
@@ -3,17 +3,16 @@ import numpy.polynomial.polynomial as poly
 import scipy as sp
 import scipy.interpolate
 import math
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
 import pickle
 import sys
 
 sys.path.append('Classes')
 from reservoir import Reservoir
-
-
 speakerN = 9
 channelsN = 12
+samplesN = 30
 trialsN = 1
 N = 10
 
@@ -23,7 +22,9 @@ with open('prepdata.pickle', 'rb') as f:
 trainData = data['train']
 testData = data['test']
 
+# speakerN x (Nodes x (Timesteps*samplesN))
 stateData = []
+# speakerN x (Channels x (Timesteps*noSamples))
 inputData = []
 for trial in range(trialsN):
 	r = Reservoir(N = N, inputScaling=0.2, biasScaling=1, inputDim=12)
@@ -32,7 +33,25 @@ for trial in range(trialsN):
 		stateData.append(state)
 		inputData.append(inp)
 
+	#Ridge Regression with different regularizers (Tikhonov Alpha)
+#	alphas = 2.0**np.arange(-10,0)
+#	for candidateAlpha in alphas:
+#		mse = 0		
+		#n-fold crossvalidation
+#		n = 5
+#		foldSize = samplesN/n  
+#		for fold in range(n):
+#			trainInds = [list(range(1,(fold - 1) * foldSize + 1)),list(range(fold * foldSize + 1, samplesN + 1))]
+#			trainExamples = [stateData[(fold - 1) * (foldSize + 1):fold * foldSize + 1)]]
 
+	samplesPerSpeaker = []
+	for speakerSamples in trainData:
+		speakerInput = np.array(speakerSamples)
+		speakerInput = speakerInput.reshape((120,12))
+		samplesPerSpeaker.append(speakerInput)
+
+	r.loadingAndConceptors(patterns=samplesPerSpeaker, t_learn=120, t_washout=0, mode = 'batch')
+	print(r.conceptors[1].shape)
 		
 
 
